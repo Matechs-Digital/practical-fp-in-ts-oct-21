96ebc7d7823e3eab1e431032e3d4f5d22779f8c7
Michael Arnaldi <arnaldimichael@gmail.com>
2021-10-19 17:51:07 +0100

    solution-pipeable

--- src/exercises/day-1/01-pure.ts
+++ src/exercises/day-1/01-pure.ts
@@ -1,49 +1,10 @@
-/**
- * Theory:
- *
- * Intro to pure functions and functional programming in general.
- *
- * In this module we introduce the basic concepts of functional programming and we take a look at the style of APIs that we will encounter later on.
- */
+import { pipe } from "@effect-ts/system/Function"
 
-/**
- * Segment:
- *
- * Fluent vs Pipeable
- */
+import * as MathExpr from "./01-MathExpr"
 
-//
-// example code with simple identity
-//
-
-/**
- * Exercise:
- *
- * Build a module Math with 4 behaviour fuctions `add`, `mul`, `sub`, `div` that acts on numbers both in pipeable and fluent variants
- */
-
-class MathExpr {
-  static fromNumber(n: number): MathExpr {
-    return new MathExpr(n)
-  }
-
-  constructor(readonly n: number) {}
-
-  add(this: MathExpr, that: MathExpr) {
-    return new MathExpr(this.n + that.n)
-  }
-
-  mul(this: MathExpr, that: MathExpr) {
-    return new MathExpr(this.n * that.n)
-  }
-
-  div(this: MathExpr, that: MathExpr) {
-    return new MathExpr(this.n / that.n)
-  }
-
-  get(this: MathExpr): number {
-    return this.n
-  }
-}
-
-MathExpr.fromNumber(0).add(MathExpr.fromNumber(0)).get()
+const x = pipe(
+  MathExpr.fromNumber(0),
+  MathExpr.add(MathExpr.fromNumber(0)),
+  MathExpr.mul(MathExpr.fromNumber(0)),
+  MathExpr.get
+)
