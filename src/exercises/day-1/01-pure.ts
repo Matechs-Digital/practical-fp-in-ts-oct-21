ffc6ffe91bd82b1df1683a9b9430838757e66292
Michael Arnaldi <arnaldimichael@gmail.com>
2021-10-19 17:41:49 +0100

    solutions-1

--- src/exercises/day-1/01-pure.ts
+++ src/exercises/day-1/01-pure.ts
@@ -21,3 +21,29 @@
  *
  * Build a module Math with 4 behaviour fuctions `add`, `mul`, `sub`, `div` that acts on numbers both in pipeable and fluent variants
  */
+
+class MathExpr {
+  static fromNumber(n: number): MathExpr {
+    return new MathExpr(n)
+  }
+
+  constructor(readonly n: number) {}
+
+  add(this: MathExpr, that: MathExpr) {
+    return new MathExpr(this.n + that.n)
+  }
+
+  mul(this: MathExpr, that: MathExpr) {
+    return new MathExpr(this.n * that.n)
+  }
+
+  div(this: MathExpr, that: MathExpr) {
+    return new MathExpr(this.n / that.n)
+  }
+
+  get(this: MathExpr): number {
+    return this.n
+  }
+}
+
+MathExpr.fromNumber(0).add(MathExpr.fromNumber(0)).get()
