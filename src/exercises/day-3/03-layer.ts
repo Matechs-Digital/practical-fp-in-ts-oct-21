b3c87129d7a8ab251fe0a0215ecf3a638c5ea685
Michael Arnaldi <arnaldimichael@gmail.com>
2021-10-21 21:23:10 +0100

    up to layers

--- src/exercises/day-3/03-layer.ts
+++ src/exercises/day-3/03-layer.ts
@@ -16,19 +16,81 @@
  * >+> operator
  * <+< operator
  */
+import { Tagged } from "@effect-ts/core/Case"
 import * as T from "@effect-ts/core/Effect"
 import * as L from "@effect-ts/core/Effect/Layer"
+import { pipe } from "@effect-ts/core/Function"
+import { tag } from "@effect-ts/core/Has"
+import type { _A } from "@effect-ts/system/Utils"
 
-import { ConsoleService } from "./02-services"
+export const makeLiveConsole = T.succeedWith(() => ({
+  log: (msg: string) =>
+    T.succeedWith(() => {
+      console.log(msg)
+    })
+}))
 
-export const LiveConsole = L.fromEffect(ConsoleService)(
+export interface ConsoleService extends _A<typeof makeLiveConsole> {}
+
+export const ConsoleService = tag<ConsoleService>()
+
+export const LiveConsole = L.fromEffect(ConsoleService)(makeLiveConsole)
+
+export const { log } = T.deriveLifted(ConsoleService)(["log"], [], [])
+
+export interface LoggerService {
+  info(msg: string): T.Effect<unknown, never, void>
+}
+
+export const LoggerService = tag<LoggerService>()
+export const { info } = T.deriveLifted(LoggerService)(["info"], [], [])
+
+export interface RandomGeneratorService {
+  random: T.Effect<unknown, never, number>
+}
+
+export const RandomGeneratorService = tag<RandomGeneratorService>()
+export const { random } = T.deriveLifted(RandomGeneratorService)([], ["random"], [])
+
+export class InvalidRandom extends Tagged("InvalidRandom")<{
+  readonly invalidRandom: number
+}> {}
+
+export const program = T.gen(function* (_) {
+  const x = yield* _(random)
+
+  yield* _(
+    T.if_(
+      x >= 0.5,
+      () => info(`got number: ${x}`),
+      () => T.fail(new InvalidRandom({ invalidRandom: x }))
+    )
+  )
+
+  return x
+})
+
+export const LiveRandom = L.fromEffect(RandomGeneratorService)(
   T.succeedWith(() => ({
-    log: (msg) =>
-      T.succeedWith(() => {
-        console.log(msg)
-      })
+    random: T.succeedWith(() => Math.random())
   }))
 )
+
+export const LiveLogger = L.fromEffect(LoggerService)(
+  T.gen(function* (_) {
+    const { log } = yield* _(ConsoleService)
+
+    return {
+      info: (msg) => log(`[info]: ${msg}`)
+    }
+  })
+)
+
+export const ConsoleBasedLogger = LiveConsole[">>>"](LiveLogger)
+
+export const AppDependencies = LiveRandom["+++"](ConsoleBasedLogger)
+
+export const main = pipe(program, T.provideSomeLayer(AppDependencies), T.runPromise)
 
 /**
  * Exercise:
