2739aadc3ac49573b2243a22c51a5d40295fc5c3
Michael Arnaldi <arnaldimichael@gmail.com>
2021-10-22 16:45:47 +0100

    use toLayer as alternative

--- src/exercises/day-3/03-layer.ts
+++ src/exercises/day-3/03-layer.ts
@@ -51,7 +51,7 @@ export interface ConsoleService extends _A<typeof makeLiveConsole> {}
 
 export const ConsoleService = tag<ConsoleService>()
 
-export const LiveConsole = L.fromEffect(ConsoleService)(makeLiveConsole)
+export const LiveConsole = pipe(makeLiveConsole, T.toLayer(ConsoleService))
 
 export const { log } = T.deriveLifted(ConsoleService)(["log"], [], [])
 
