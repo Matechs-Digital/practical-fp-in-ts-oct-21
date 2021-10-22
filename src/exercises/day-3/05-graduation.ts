352006325b845afca274b654d0b33f1034393f81
Michael Arnaldi <arnaldimichael@gmail.com>
2021-10-22 18:22:21 +0100

    start move

--- src/exercises/day-3/05-graduation.ts
+++ src/exercises/day-3/05-graduation.ts
@@ -8,6 +8,7 @@
  * https://github.com/Matechs-Garage/mars-rover-kata
  */
 import { Tagged } from "@effect-ts/core/Case"
+import { hole } from "@effect-ts/system/Function"
 
 export interface IntBrand {
   readonly Int: unique symbol
@@ -19,13 +20,13 @@ export interface WidthBrand {
   readonly Width: unique symbol
 }
 
-export type Width = number & WidthBrand
+export type Width = Int & WidthBrand
 
 export interface HightBrand {
   readonly Hight: unique symbol
 }
 
-export type Hight = number & HightBrand
+export type Hight = Int & HightBrand
 
 export class GridSize extends Tagged("GridSize")<{
   readonly width: Width
@@ -47,13 +48,13 @@ export interface PositionXBrand {
   readonly PositionX: unique symbol
 }
 
-export type PositionX = number & PositionXBrand
+export type PositionX = Int & PositionXBrand
 
 export interface PositionYBrand {
   readonly PositionY: unique symbol
 }
 
-export type PositionY = number & PositionYBrand
+export type PositionY = Int & PositionYBrand
 
 export class Position extends Tagged("Position")<{
   readonly x: PositionX
@@ -71,3 +72,41 @@ export class GoForward extends Tagged("GoForward")<{}> {}
 export class GoBackward extends Tagged("GoBackward")<{}> {}
 
 export type Command = TurnLeft | TurnRight | GoForward | GoBackward
+
+export function mod(x: Int, y: Int): Int {
+  return Math.abs(x % y) as Int
+}
+
+export function move(rover: Rover, planet: Planet, command: Command): Rover {
+  switch (command._tag) {
+    case "GoForward": {
+      switch (rover.orientation._tag) {
+        case "North": {
+          return rover.copy({
+            position: rover.position.copy({
+              y: mod((rover.position.y + 1) as Int, planet.gridSize.hight) as PositionY
+            })
+          })
+        }
+        case "Est": {
+          return hole()
+        }
+        case "South": {
+          return hole()
+        }
+        case "West": {
+          return hole()
+        }
+      }
+    }
+    case "GoBackward": {
+      return hole()
+    }
+    case "TurnLeft": {
+      return hole()
+    }
+    case "TurnRight": {
+      return hole()
+    }
+  }
+}
